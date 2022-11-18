24fe0e0d9c3140631f05e4717a527bab22139256
zhangyajun <zhangyajun_yj@163.com>
2022-11-18 14:17:57 +0800

    忽略文件删除

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        
         System.out.println("Hello, World LeetCode!");
+
+        int[] nums = {3,4,5,1,2};
+        Solution153.findMin(nums);
+
+        System.out.println(Solution153.findMin(nums));
+        
     }
 }
--- src/Solution153.java
+++ src/Solution153.java
@@ -1,5 +1,5 @@
-class Solution {
-    public int findMin(int[] nums) {
+class Solution153 {
+    public static int findMin(int[] nums) {
         // 寻找旋转排序数组中的最小值
         /*
             已知一个长度为 n 的数组，预先按照升序排列，经由 1 到 n 次 旋转 后，得到输入数组。例如，原数组 nums = [0,1,2,4,5,6,7] 
@@ -14,10 +14,11 @@ class Solution {
             输出：1
             解释：原数组为 [1,2,3,4,5] ，旋转 3 次得到输入数组。
           */
+          
         int low = 0;
         int high = nums.length - 1;
         while(low < high){
-            int pivot = low +（high - low）/ 2;
+            int pivot = low +(high - low)/ 2;
             if(nums[pivot] < nums[high]){
                 high = pivot;
             }else {
