c20640369737e08d36f0866b269f016d4813186c
zhangyajun <zyjjfm@gmail.com>
2023-03-16 16:02:11 +0800

    17

--- src/App.java
+++ src/App.java
@@ -4,8 +4,7 @@ public class App {
         System.out.println("Hello, World LeetCode!");
 
         int[] nums = {3,4,5,1,2};
-
-
-        //  test
+        
+        Solution17.letterCombinations(null);
     }
 }
