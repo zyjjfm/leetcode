972b54787751e8cff343f9383e8f01417c538325
zhangyajun <zyjjfm@gmail.com>
2023-03-24 16:49:39 +0800

    ------

--- src/App.java
+++ src/App.java
@@ -4,11 +4,7 @@ public class App {
         
         System.out.println("Hello, World LeetCode!");
         int[] nums = {3,4,5,1,2};
-
-        // 时间戳转换
-        
-        
-
+    
         
     }
 }
--- src/Solution11.java
+++ src/Solution11.java
@@ -1,11 +1,10 @@
 public class Solution11 {
-
+    
     // 11. 盛最多水的容器
     // https://leetcode-cn.com/problems/container-with-most-water/
     public int maxArea(int[] height) {
         int i = 0,j = height.length - 1, res = 0;
          while(i < j){
-
             res = height[i] < height[j] ? 
             Math.max(res, (j - i) * height[i++]) :
             Math.max(res, (j - i) * height[j--]);
