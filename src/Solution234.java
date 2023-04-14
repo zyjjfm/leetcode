416302de2bd33442e2551985e4c5db934866a41b
zhangyajun <zyjjfm@gmail.com>
2023-04-14 15:55:58 +0800

    234

---
+++ src/Solution234.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution234 {
+    
+    // 234. 给你一个单链表的头节点 head ，请你判断该链表是否为回文链表。
+    // 如果是，返回 true ；否则，返回 false 。
+
+    public boolean isPalindrome(ListNode head) {
+
+        List<Integer> vals = new ArrayList<Integer>();
+        //将链表转成数组
+        ListNode currentNode = head;
+        while(currentNode != null){
+            vals.add(currentNode.val);
+            currentNode = currentNode.next;
+        }
+
+        //使用双指针判断是否回文
+        int front = 0;
+        int back = vals.size() - 1;
+        while(front < back){
+            if(!vals.get(front).equals(vals.get(back))){
+                return false;
+            }
+            front ++;
+            back --;
+        }
+        return true;
+    }
+}
+
--- src/Solution69.java
+++ src/Solution69.java
@@ -1,7 +1,7 @@
 public class Solution69 {
     // 给你一个非负整数 x ，计算并返回 x 的 算术平方根 。 由于返回类型是整数，结果只保留 整数部分 ，小数部分将被 舍去 。  注意：不允许使用任何内置的库函数，如  sqrt 。 
 
-
+    
     public int mySqrt(int x) {
         if(x == 0){
             return 0;
