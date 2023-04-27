a9fa64a797bb07bdf1089c2cddc59fe56916d223
zhangyajun <zyjjfm@gmail.com>
2023-04-27 11:23:44 +0800

    459

--- src/Solution459.java
+++ src/Solution459.java
@@ -4,16 +4,18 @@ public class Solution459 {
     // 给定一个非空的字符串 s ，检查是否可以通过由它的一个子串重复多次构成。
     public  boolean repeatedSubstringPattern(String s) {
         int n = s.length();
-        for(int i= 1;i * 2 <= n;++i){
-            boolean match = true;
-            for(int j = i;j < n; ++j){
-                if(s.charAt(j) != s.charAt(j-i)){
-                    match = false;
-                    break;
+        for (int i = 1; i * 2 <= n; ++i) {
+            if (n % i == 0) {
+                boolean match = true;
+                for (int j = i; j < n; ++j) {
+                    if (s.charAt(j) != s.charAt(j - i)) {
+                        match = false;
+                        break;
+                    }
                 }
-            }
-            if(match){
-                return true;
+                if (match) {
+                    return true;
+                }
             }
         }
         return false;
