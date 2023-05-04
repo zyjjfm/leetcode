9c58e1ca4d2c68bfd4117249ad40dd4c3a231147
zhangyajun <zyjjfm@gmail.com>
2023-05-04 19:10:32 +0800

    367

---
+++ src/Solution367.java
@@ -0,0 +1,26 @@
+
+import java.util.ArrayList;
+
+public class Solution367 {
+    /*
+     367. 有效的完全平方数
+    */
+    public boolean isPerfectSquare(int num){
+
+      int left = 0, right = 0;
+      while(left <= right ){
+
+        int mid = (right - left )/2 + left;
+        long square = (long) mid * mid;
+  
+        if(square < num){
+          left = mid +1;
+        }else if(square > num){
+          right = mid -1;
+        }else{
+          return true;
+        }
+      }
+      return false;
+    }
+  }
