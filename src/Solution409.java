e937b86a545832942e40067803b32a7638081216
zhangyajun <zyjjfm@gmail.com>
2023-06-27 17:43:55 +0800

    409

---
+++ src/Solution409.java
@@ -0,0 +1,25 @@
+public class Solution409 {
+    
+    /**
+     * 
+     409. 最长回文串
+
+     给定一个包含大写字母和小写字母的字符串 s ，返回 通过这些字母构造成的 最长的回文串 。
+     在构造过程中，请注意 区分大小写 。比如 "Aa" 不能当做一个回文字符串。
+
+
+    */
+     public int longestPalindrome(String s) {
+
+            int[] count = new int[128];
+            int length = s.length();
+
+            for
+
+
+
+
+
+        return 0;
+    }
+}
