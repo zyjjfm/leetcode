4af1a18836c96919c1d6cf5905b8eb356ceb7a74
zhangyajun <zyjjfm@gmail.com>
2023-07-11 17:33:03 +0800

    257

--- src/Solution257.java
+++ src/Solution257.java
@@ -1,9 +1,20 @@
+import java.util.List;
+
 public class Solution257 {
      // 257. 二叉树的所有路径
     /**
      * 给你一个二叉树的根节点 root ，按 任意顺序 ，返回所有从根节点到叶子节点的路径。 
      * 叶子节点 是指没有子节点的节点。
     */
+    public List<String> binaryTreePaths(TreeNode root){
+        List<String> paths = new ArrayList<String>();
+        constructPaths();
+        return paths;
+    }
+
+
+
+
     public void constructPaths(TreeNode root,String path,List<String> paths){
 
         if(root != null){
@@ -16,7 +27,6 @@ public class Solution257 {
                 pathSB.append("->");
                 constructPaths(root.left, pathSB.toString(), paths);
                 constructPaths(root.right, pathSB.toString(), paths);
-
             }
         }
     }    
