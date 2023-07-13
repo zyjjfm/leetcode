c18fcc6f24b0a2b44b959967e2ff3850b117644e
zhangyajun <zyjjfm@gmail.com>
2023-07-13 10:23:52 +0800

    1603

---
+++ src/Solution1603.java
@@ -0,0 +1,32 @@
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class Solution1603 {
+
+    int big,medium,small;
+
+    // 设计停车系统
+    public ParkingSystem(int big, int medium, int small) {
+        this.big = big;
+        this.medium = medium;
+        this.small = small;
+    }
+    public boolean addCar(int carType) {
+        if(carType == 1){
+            if(big > 0){
+                big--;
+                return true;
+            }
+        }else if(carType == 2){
+
+            if(){
+                
+            }
+
+        }else if(carType == 3){
+
+        }
+    }
+    }
