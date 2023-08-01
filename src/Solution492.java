04235d9300296561cc7e9bfcf58c2ec260adb31d
zhangyajun <zyjjfm@gmail.com>
2023-08-01 15:39:56 +0800

    492

---
+++ src/Solution492.java
@@ -0,0 +1,16 @@
+public class Solution492 {
+
+    // 492. 构造矩形
+    public int[] constructRectangle(int area) {
+    
+
+        int w = (int)Math.sqrt(area);
+
+        while(area % w != 0){
+            --w;
+        }
+
+        
+        return new int[]{ area / w,w};
+    }
+}
