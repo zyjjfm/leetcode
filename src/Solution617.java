67dc7da4b78c12465126bd7e71f57f33f3480716
zhangyajun <zyjjfm@gmail.com>
2023-08-14 18:28:49 +0800

    617

--- src/Solution617.java
+++ src/Solution617.java
@@ -11,11 +11,19 @@ public class Solution617 {
      */
 
     public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if(root1 == null){
+            return root2;
+        }
+        if(root2 == null){
+            return root1;
+        }
 
+        TreeNode merged = new TreeNode(root1.val + root2.val);
 
-        
+        merged.left = mergeTrees(root1.left, root2.left);
+        merged.right = mergeTrees(root1.right, root2.right);
 
-        return root1;
+        return merged;
     }
 
 }
