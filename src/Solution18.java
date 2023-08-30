b642a141663d225672678de3ba2fc0d7f6860377
zhangyajun <zyjjfm@gmail.com>
2023-08-30 17:56:30 +0800

    18

--- src/Solution18.java
+++ src/Solution18.java
@@ -9,18 +9,15 @@ public class Solution18 {
     //18. 四数之和
 
     /*
-
     给你一个由 n 个整数组成的数组 nums ，和一个目标值 target 。
     请你找出并返回满足下述全部条件且不重复的四元组 [nums[a], nums[b], nums[c], nums[d]] 
-    （若两个四元组元素一一对应，则认为两个四元组重复）：
-
+   （若两个四元组元素一一对应，则认为两个四元组重复）：
     0 <= a, b, c, d < n
     a、b、c 和 d 互不相同
     nums[a] + nums[b] + nums[c] + nums[d] == target
     你可以按 任意顺序 返回答案 。
     */
-    
-  public static List<Integer>  fourSum(int[] digits，int target){
+    public static List<List<Integer>>  fourSum(int[] nums, int target){
 
     List<List<Integer>> quadruplets = new ArrayList<List<Integer>>();
 
@@ -28,8 +25,8 @@ public class Solution18 {
       return quadruplets;
     }
 
-    Arrays.sort(nums);
-    int length = nums.length;
+     Arrays.sort(nums);
+     int length = nums.length;
 
     for(int i = 0;i<length - 3;i++){
 
@@ -42,6 +39,16 @@ public class Solution18 {
       if((long)nums[i] + nums[length - 3] + nums[length - 2] + nums[length - 1] < target){
         continue;
       }
+       for (int j = i + 1; j < length - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                if ((long) nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) {
+                    break;
+                }
+                if ((long) nums[i] + nums[j] + nums[length - 2] + nums[length - 1] < target) {
+                    continue;
+                }
       int left = j + 1;
       int right = length - 1;
 
@@ -69,7 +76,6 @@ public class Solution18 {
           right--;
         }
       }
-    }
+    }  }
     return quadruplets;
-  }
-}
+  }}
