db22477eb60a9da32b1572513283967d2d583a38
zhangyajun <zyjjfm@gmail.com>
2023-09-11 13:55:32 +0800

    530

--- src/Solution401.java
+++ src/Solution401.java
@@ -12,10 +12,9 @@ public class Solution401 {
         for(int h = 0;h < 12;++h){
             for(int m = 0;m < 60;++m){
                 if(Integer.bitCount(h)+Integer.bitCount(m) == turnedOn){
-                    
+                    ans.add(h + ":" +(m < 10 ? "0" : "") + m);
                 }
             }
-
         }
         return ans;
     }
---
+++ src/Solution530.java
@@ -0,0 +1,28 @@
+public class Solution530 {
+
+   int pre;
+   int ans;
+
+    // 530. 二叉搜索树的最小绝对差
+      public int getMinimumDifference(TreeNode root) {
+
+        ans = Integer.MAX_VALUE;
+        pre = -1;
+        dfs(root);
+        return ans;
+    }
+
+    public void dfs(TreeNode root){
+      if( root == null){
+        return;
+      }
+      dfs(root.left);
+      if(pre == -1){
+        pre = root.val;
+      }else{
+        ans = Math.min(ans, root.val - pre);
+        pre = root.val;
+      }
+      dfs(root);
+    }
+}
