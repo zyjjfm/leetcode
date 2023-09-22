e2847c77fc3b31cccdc613df7c3d0e57c9435c8e
zhangyajun <zyjjfm@gmail.com>
2023-09-22 14:51:50 +0800

    1748

---
+++ src/Solution1748.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution1748 {
+    //唯一元素的和 
+    public int sumOfUnique(int[] nums) {
+
+        Map<Integer,Integer> cnt = new HashMap<Integer,Integer>();
+        for(int num:nums){
+            ctn.put(num,cnt.getOrDefault(num,0)+1);
+        }
+        int ans = 0;
+
+        for(Map.Entry<Integer,Integer> entry:cnt.entrySet()){
+
+            int num = entry.getKey(),c = entry.getValue();
+            if(c == 1){
+                ans += num;
+            }
+        }
+        return ans;
+    }
+}
+
+
+
+
+
+
