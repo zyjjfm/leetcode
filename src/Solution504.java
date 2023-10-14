8cd533f451dd29f9e380f54dfffaf44dc42003c2
zhangyajun <zyjjfm@gmail.com>
2023-10-14 23:54:22 +0800

    504

---
+++ src/Solution504.java
@@ -0,0 +1,25 @@
+public class Solution504 {
+    
+    // 给定一个整数 num，将其转化为 7 进制，并以字符串形式输出。
+    // 504  七进制  
+
+    public String convertToBase7(int num) {
+        if(num == 0){
+
+            return "0";
+        }
+       
+        boolean nevat
+
+
+
+
+    return "";
+    }
+}
+
+
+
+
+
+
