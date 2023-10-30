85fdd76fb509f86466cb0f1d056a8ac10a89ebdf
zhangyajun <zyjjfm@gmail.com>
2023-10-30 11:34:35 +0800

    122

--- src/Solution121.java
+++ src/Solution121.java
@@ -11,14 +11,13 @@ public class Solution121 {
 
     public int maxProfit(int[] prices) {
         int maxProfit = 0;
-
         for(int i = 0;i<prices.length;i++){
             for(int j = i+1;j < prices.length;j++){
 
                 int profit = prices[j] - prices[i];
                 
                 if(profit > maxProfit){
-                    
+                    maxProfit = profit;
                 }
             }
         }
