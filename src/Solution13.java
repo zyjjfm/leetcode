8cf26aa55a0c934ef07d55787a2147ae58d130ff
zhangyajun <zyjjfm@gmail.com>
2023-11-20 11:05:57 +0800

    13

---
+++ src/Solution13.java
@@ -0,0 +1,47 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution13 {
+
+    // 13. 罗马数字转整数         
+    // https://leetcode.cn/problems/roman-to-integer/description/?envType=study-plan-v2&envId=top-interview-150
+
+      Map<Character,Integer> symbolValues = new HashMap<Character,Integer>(){{
+            put('I',1);
+            put('V',5);
+            put('X',10);
+            put('L',50);
+            put('C',100);
+            put('D',500);
+            put('M',1000);
+        }};
+
+
+    public int romanToInt(String s){
+
+        int ans = 0;
+        int n = s.length();
+
+        for(int i = 0;i< n;++i){    
+
+            int value = symbolValues.get(s.charAt(i));
+
+            if(i < n -1 && value < symbolValues.get(s.charAt(i+i))){
+
+                ans -= value;
+
+            }else{
+
+
+            }
+            
+
+
+
+
+
+        }
+        
+        return ans;
+    }
+}
--- src/Solution42.java
+++ src/Solution42.java
@@ -20,6 +20,12 @@ public class Solution42 {
             rightMax[i] = Math.max(rightMax[i+1], height[i]); 
         }
 
+        int[] rightMax = new int[n];
+        rightMax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; --i) {
+            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+        }
+        
         int ans = 0;
         for(int i = 0;i < n;++i){
             ans += Math.min(leftMax[i],rightMax[i]) -height[i];
