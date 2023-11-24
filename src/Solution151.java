397a834ccd060e1326225a3d8130dca3f93ca158
zhangyajun <zyjjfm@gmail.com>
2023-11-24 17:11:20 +0800

    151

---
+++ src/Solution151.java
@@ -0,0 +1,22 @@
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Solution151 {
+    
+    // 151. 反转字符串中的单词
+    
+    // 翻转字符串里的单词 
+
+    public String reverseWords(String s) {
+
+        // 除去开头和末尾的空白字符
+        s = s.trim();
+        // .asList(s.split("\\s+"));
+        List<String> wordList = Arrays.asList(s.split("\\s+"));
+
+        Collections.reverse(wordList);
+
+        return String.join(" ", wordList);
+    }
+}
