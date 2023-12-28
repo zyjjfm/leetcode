bcfcb15cbb2e756d1e8a640410cd89b49d910ac4
zhangyajun <zyjjfm@gmail.com>
2023-12-28 13:56:34 +0800

    56

---
+++ src/Solution56.java
@@ -0,0 +1,57 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Solution56 {
+    
+    public int[][] merge(int[][] intervals) {
+
+        if(intervals.length == 0){
+
+            return new int[0][2];
+        }
+
+        Arrays.sort(intervals,new Comparator<int[]>() {
+
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+
+        List<int[]> merged = new ArrayList<int[]>();
+
+        for(int i = 0;i < intervals.length;++i){
+
+            int L = intervals[i][0];
+            int R = intervals[i][1];
+
+            if(merged.size() == 0 || merged.get(merged.size() - 1)[1] < L){
+            
+            }
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+        
+
+        return new int[][]{};
+    }
+}
+
+
+
+
+
+
