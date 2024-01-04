d79d60d6e3f17680068f22fa64397f4a5a98d95b
zhangyajun <zyjjfm@gmail.com>
2024-01-04 16:56:50 +0800

    71

--- src/Solution71.java
+++ src/Solution71.java
@@ -6,17 +6,15 @@ public class Solution71 {
     /**
      * 给你一个字符串 path ，表示指向某一文件或目录的 Unix 风格 绝对路径 （以 '/' 开头），请你将其转化为更加简洁的规范路径。
      * 
-     * 
-     * 
      */
 
     public String simplifyPath(String path) {
 
         String[] names = path.split("/");
+        
         Deque<String> stack = new ArrayDeque<String>();
 
         for (String name : names) {
-
             if ("..".equals(name)) {
                 if (!stack.isEmpty()) {
                     stack.pollLast();
@@ -28,13 +26,9 @@ public class Solution71 {
 
         StringBuffer ans = new StringBuffer();
         if(stack.isEmpty()){
-
             ans.append('/');
-
         }else {
-
             while(!stack.isEmpty()){
-
                 ans.append('/');
                 ans.append(stack.pollFirst());
             }
